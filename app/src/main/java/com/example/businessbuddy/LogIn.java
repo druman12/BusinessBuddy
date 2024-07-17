31c14a25519938f156e39e0057afd0f3d2a0ab2c
vivek77066 <153838967+vivek77066@users.noreply.github.com>
2024-07-17 23:23:51 +0530

    design done

--- app/src/main/java/com/example/businessbuddy/LogIn.java
+++ app/src/main/java/com/example/businessbuddy/LogIn.java
@@ -1,6 +1,10 @@
 package com.example.businessbuddy;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LogIn extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        TextView signup=findViewById(R.id.signup);
+
+    signup.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startActivity(new Intent(LogIn.this,SignUp.class));
+        }
+    });
+
+
     }
 }
--- app/src/main/java/com/example/businessbuddy/SignUp.java
+++ app/src/main/java/com/example/businessbuddy/SignUp.java
@@ -1,6 +1,10 @@
 package com.example.businessbuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,13 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView changeto = findViewById(R.id.already);
+        changeto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUp.this, LogIn.class));
+            }
         });
     }
 }
